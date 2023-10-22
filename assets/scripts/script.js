27a1face4d51c96a2c8d5f2908efaf72efb489e5
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-10-22 20:51:12 -0300

    Almost finishing the functionalities

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -9,17 +9,32 @@ const number1 = Math.ceil(Math.random()*10)
 const number2 = Math.ceil(Math.random()*10)
 const correctAnswer = number1 * number2;
 
+let score = JSON.parse(localStorage.getItem("score"));
+if(!score) {
+  score = 0;
+}
 
 formEl.addEventListener("submit", () => {
   const userAnswer = +inputEl.value;
-  
+  if(userAnswer === correctAnswer) {
+    score++;
+    updateLocalStorage()
+  } else {
+    score--
+    updateLocalStorage()
+  }
 })
+
+function updateScore() {
+  scoreHeading.innerHTML = `SCORE: ${score}`
+}
 
 function getRandomNumbers(text) {
  return calculationHeading.innerText = `${number1} x ${number2} = ?`
 }
 
-// function getAnswer(inputValue) {
-// }
+function updateLocalStorage() {
+  localStorage.setItem("score", JSON.stringify(score))
+}
 
 getRandomNumbers(calculationHeading);
