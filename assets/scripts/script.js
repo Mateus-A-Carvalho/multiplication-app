16d5d558eb8b67fff98c4cd392a1d7683b92f616
Mateus A. Carvalho <mateusabreu94@hotmail.com>
2023-10-22 21:49:18 -0300

    Finished this project In this project, I learned about LocalStorage, some Math() methods and about 'casting'. I styled on my own and hope back again to add some functionalities about time of answer, other operations and fifficulty.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -10,9 +10,10 @@ const number2 = Math.ceil(Math.random()*10)
 const correctAnswer = number1 * number2;
 
 let score = JSON.parse(localStorage.getItem("score"));
-if(!score) {
-  score = 0;
-}
+if(!score) score = 0;
+if(score < 0) scoreHeading.classList.add('bad');
+
+scoreHeading.innerHTML = `SCORE: ${score}`;
 
 formEl.addEventListener("submit", () => {
   const userAnswer = +inputEl.value;
@@ -25,9 +26,6 @@ formEl.addEventListener("submit", () => {
   }
 })
 
-function updateScore() {
-  scoreHeading.innerHTML = `SCORE: ${score}`
-}
 
 function getRandomNumbers(text) {
  return calculationHeading.innerText = `${number1} x ${number2} = ?`
